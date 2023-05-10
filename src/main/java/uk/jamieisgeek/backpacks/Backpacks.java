02f50e8defc971819cc890812ba65ae32a39737b
JamieOnSootMC <jamie@sootmc.net>
2023-05-10 20:32:03 +0100

    init

---
+++ src/main/java/uk/jamieisgeek/backpacks/Backpacks.java
@@ -0,0 +1,17 @@
+package uk.jamieisgeek.backpacks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Backpacks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
